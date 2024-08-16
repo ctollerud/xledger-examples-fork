53d881214fa2673c0d30024b1fa0bb477def23f4
Corey Tollerud <ctollerud@tollerudsoftware.com>
2024-08-16 14:12:29 +0100

    crude customizations for testing via Corey's environment.
    Modified the webhook endpoint handler so that it just returns 200 without
    doing anything to avoid unnecessary variables.  Added some debugging
    middleware at the entrypoint of the webhook server.
    
    Modified config to use my own cloudflare tunnel (tied to the webhook server
    from the example project)

--- Webhooks-CSharp/GraphQL/WebServer.cs
+++ Webhooks-CSharp/GraphQL/WebServer.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Serilog;
 
@@ -19,24 +20,47 @@ namespace Webhooks.GraphQL {
         ) {
             var builder = WebApplication.CreateBuilder();
             builder.Host.UseSerilog(Log.Logger);
-            builder.WebHost.UseUrls(urls);
+
+            //COREY Customized so I can test more easilly
+            builder.WebHost.UseUrls("http://*:5005");
 
             var app = builder.Build();
+
+
+            //verifying that nothing is hitting the server at all
+            app.Use( next => async context => {
+
+                Log.Information( "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" );
+                Log.Information( "Request entered Webhook server" );
+                Log.Information( "PATH: " + context.Request.Path );
+                Log.Information( "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" );
+
+                await next( context );
+            } );
+
             app.MapGet("/ping", () => Results.Json(new {
                 data = "pong",
                 now = DateTime.UtcNow,
             }));
             app.MapPost("/projects",
                 async (
-                    HttpRequest request,
-                    [FromHeader(Name = "X-XL-Webhook-Signature")] string sig,
-                    [FromHeader(Name = "Date")] DateTimeOffset date
+                    HttpRequest request
+                    //,
+                    //[FromHeader(Name = "X-XL-Webhook-Signature")] string sig,
+                    //[FromHeader(Name = "Date")] DateTimeOffset date
                 ) => {
-                    Log.Debug("Headers: {H}", request.Headers);
-                    using var r = new StreamReader(request.Body);
-                    var body = await r.ReadToEndAsync();
-                    var req = new WebhookRequest(date, sig, body);
-                    return await handleProjectsMessage(req);
+                    Log.Information("Acessed Hebhook!!!!!!!!!!!!!!!!");
+                    Log.Information("Headers: {H}", request.Headers);
+
+
+                    //Corey's Debugging customization:
+                    return Results.Ok();
+
+
+                    //using var r = new StreamReader(request.Body);
+                    //var body = await r.ReadToEndAsync();
+                    //var req = new WebhookRequest(date, sig, body);
+                    //return await handleProjectsMessage(req);
                 });
 
             await app.StartAsync(tok);
